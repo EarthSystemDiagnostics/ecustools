39aa8067af009d1e762e2daad1afa5e0b1f8eec1
andrewdolman <andydolman@gmail.com>
2019-01-24 23:21:08 +0100

    re-export orbital forcing functions

---
+++ R/__reexports.R
@@ -0,0 +1,32 @@
+# re-export orbital forcing functions
+
+#' @importFrom orbitalforcing AnnualInsolation
+#' @name AnnualInsolation
+#' @rdname AnnualInsolation
+#' @export
+NULL
+
+#' @importFrom orbitalforcing DailyInsolation
+#' @name DailyInsolation
+#' @rdname DailyInsolation
+#' @export
+NULL
+
+
+#' @importFrom orbitalforcing DailyInsolationParam
+#' @name DailyInsolationParam
+#' @rdname DailyInsolationParam
+#' @export
+NULL
+
+#' @importFrom orbitalforcing EstimateTransferfunctionInsolation
+#' @name EstimateTransferfunctionInsolation
+#' @rdname EstimateTransferfunctionInsolation
+#' @export
+NULL
+
+#' @importFrom orbitalforcing SimulateYearFromInsolation
+#' @name SimulateYearFromInsolation
+#' @rdname SimulateYearFromInsolation
+#' @export
+NULL
