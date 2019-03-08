cedfe3957842e3597512000cb5db89ba98ec4ce1
Andrew Dolman <andydolman@gmail.com>
2019-03-08 12:18:26 +0100

    add facet_wrap_paginate_auto for multipage ggplot output

--- R/ggplot2-helpers.R
+++ R/ggplot2-helpers.R
@@ -17,3 +17,54 @@ reverselog_trans <- function(base = exp(1)) {
                     scales::log_breaks(base = base),
                     domain = c(1e-100, Inf))
 }
+
+
+
+
+#' Plot all ggforce::facet_wrap_paginate pages
+#'
+#' @description A helper function to return all pages from
+#' ggforce::facet_wrap_paginate. It automatically calculates
+#' required number of pages.
+#'
+#' @param ggplot.obj A ggplot object
+#' @param facets Faceting formula, see \code{\link{ggplot2::facet_wrap}}
+#' @param nrow number of columns per page
+#' @param ncol number of rows per page
+#'
+#' @return A list of ggplot objects
+#' @importFrom ggforce facet_wrap_paginate
+#' @export
+#'
+#' @examples
+#' gg <- facet_wrap_paginate_auto(uk37.plots.2,
+#'         facets = formula(~Location + Proxy + ID.no),
+#'         nrow = 4, ncol = 3)
+#' gg
+facet_wrap_paginate_auto <- function(ggplot.obj, facets, nrow, ncol) {
+  built.plot <- ggplot_build(ggplot.obj)
+
+  # Make robust to changes in built ggplot object stucture in dev vs. CRAN version
+  if (is.null(built.plot$layout$panel_params) == FALSE) {
+    n.pages <-
+      ceiling(length(built.plot$layout$panel_params) / (nrow * ncol))
+  } else if (is.null(built.plot$layout$panel_ranges) == FALSE) {
+    n.pages <-
+      ceiling(length(built.plot$layout$panel_ranges) / (nrow * ncol))
+  } else{
+    stop("Structure of built ggplot object has changed")
+  }
+
+  ggs <- lapply(1:n.pages, function(i) {
+    ggplot.obj +
+      ggforce::facet_wrap_paginate(
+        facets,
+        scales = "free_y",
+        labeller = labeller(.multi_line = FALSE),
+        ncol = ncol,
+        nrow = nrow,
+        page = i
+      )
+  })
+  return(ggs)
+}
