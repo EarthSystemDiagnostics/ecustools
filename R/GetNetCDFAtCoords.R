ca520b4c742368ab60e565b6aa1da601723e2d1c
andrewdolman <andydolman@gmail.com>
2020-11-18 17:38:54 +0100

    fix bug for single location request and allow different names for lat lon variables

--- R/GetNetCDFAtCoords.R
+++ R/GetNetCDFAtCoords.R
@@ -4,10 +4,13 @@
 #' @param req.coords matrix or dataframe of longitude and latitude coordinates
 #' @param req.var name of the variable to extract
 #' @param time.var name of the time variable
+#' @param lon.var,lat.var names of the longitude and latitude variables in the 
+#' netcdf file
 #'
 #' @return
 #' @export
-#'
+#' @import ncdf4
+#' @importFrom magrittr %>%
 #' @examples
 #' \dontrun{req.coors = data.frame(
 #'   lon = c(-64.5417, -64.9888, -64.7),
@@ -16,60 +19,74 @@
 #'                   req.coords = req.coords,
 #'                   req.var = "nobs")
 #' }
-GetNetCDFAtCoords <- function(filename, req.coords, req.var, time.var = "time"){
-
+GetNetCDFAtCoords <- function(filename, req.coords, req.var, time.var = "time",
+                              lon.var = "longitude", lat.var = "latitude"){
+  
   if (!requireNamespace("ncdf4", quietly = TRUE)) {
     stop("package 'ncdf4' is needed for this function to work. Please install it.
          Linux users may have to install the 3rd party libraries libnetcdf-dev
          and libnetcdff-dev before installing ncdf4",
          call. = FALSE)
   }
-
-  nc1 <- ncdf4::nc_open(filename, readunlim = FALSE)
+  
+  #browser()
+  nc1 <- nc_open(filename, readunlim = FALSE)
   print(nc1)
-
-  lats <- ncdf4::ncvar_get(nc1, varid = "latitude")
-  lons <- ncdf4::ncvar_get(nc1, varid = "longitude")
+  
+  lats <- ncdf4::ncvar_get(nc1, varid = lat.var)
+  lons <- ncdf4::ncvar_get(nc1, varid = lon.var)
   lons <- ifelse(lons > 180, lons - 360, lons)
-
+  
   nc.coords <- expand.grid(lons = lons, lats = lats)
-
+  
   ind <- sapply(1:nrow(req.coords), function(x) {
     which.min(geosphere::distHaversine(req.coords[x, ], nc.coords))
   })
-
+  
   print(ind)
-
+  
   req.nc.coords <- nc.coords[ind,]
-
+  
   lon.inds.to.get <- sapply(req.nc.coords[,1], function(x) which(lons == x))
   lat.inds.to.get <- sapply(req.nc.coords[,2], function(x) which(lats == x))
-
+  
   coord.inds.to.get <- cbind(lon.inds.to.get, lat.inds.to.get)
-
+  
   system.time(
-    dat.out <- plyr::aaply(coord.inds.to.get[,], 1, function(x) {
-      ncdf4::ncvar_get(
-        nc1,
-        varid = req.var,
-        start = c(x[1], x[2], 1),
-        count = c(1, 1, -1)
-      )},
-      .progress = "text")
+    dat.out <- coord.inds.to.get %>% 
+      tibble::as_tibble() %>% 
+      dplyr::mutate(row = 1:n()) %>% 
+      dplyr::rowwise() %>% 
+      dplyr::do({
+        tmp <- ncdf4::ncvar_get(
+          nc1,
+          varid = req.var,
+          start = c(.$lon.inds.to.get, .$lat.inds.to.get, 1),
+          count = c(1, 1, -1)
+        )
+        data.frame(location = as.character(.$row), date = 1:length(tmp),
+                   req.var = tmp)
+      })
   )
-
-  dat.out <- tibble::as_tibble(t(dat.out))
-
+  
+  #dat.out <- tibble::as_tibble(t(dat.out))
+  
   dates <- ncdf4::ncvar_get(nc1, varid = "time", start = c(1), count = c(-1))
-
+  
   ncdf4::nc_close(nc1)
-
-  dat.out$date <- dates
-
+  
   req.coords$location <- as.character(1:nrow(req.coords))
-
-  dat.out <- tidyr::gather(dat.out, location, UQ(req.var), -date)
+  
+  #dat.out <- tidyr::gather(dat.out, location, UQ(req.var), -date)
+  
   dat.out <- dplyr::left_join(req.coords, dat.out)
-
+  
+  # dat.out <- dat.out %>% 
+  #   group_by(location) %>% 
+  #   pivot_wider(names_from = location, values_from = req.var)
+  
+  dat.out$date <- rep(dates, nrow(req.coords))
+  
+  
   return(dat.out)
 }
